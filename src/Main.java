41fb0c1bfec8d81b34bacbf12c061ea5a4d970d2
MRALADAG <mstf_aladag@yahoo.com>
2022-09-18 17:31:51 +0300

    have been applied mukemmelSayi application

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 6;
+        System.out.println(findMukemmelSayi(number) ? "\nSayi mükemmel sayıdır.\n" : "\nSayı mükemmel sayı değildir.");
+
+    }
+
+    public static boolean findMukemmelSayi(int number) {
+        boolean result = false;
+        int total = 0;
+
+        if (number > 1) {
+            for (int i = 1; i < number; i++) {
+                if (number % i == 0) {
+                    total += i;
+                }
+            }
+
+            if (total == number) {
+                result = true;
+            }
+        }
+        return result;
+    }
+}
